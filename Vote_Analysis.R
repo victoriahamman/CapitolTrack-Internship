61be10b749fe8088c3383d23c1bc0e4a7afd67c8
Victoria Hamman <vee10hamman@gmail.com>
2025-11-20 11:08:52 -0800

    Add Vote_Analysis

---
+++ Vote_Analysis.R
@@ -0,0 +1,9 @@
+
+#Data Files
+Members <- read.csv("C:/Users/vee10/Downloads/CapitolTrack/Members.csv")
+Votes <- read.csv("C:/Users/vee10/Downloads/CapitolTrack/Votes.csv")
+
+#Libraries
+library(RSQLite)
+library(tidyr)
+library(dplyr)
