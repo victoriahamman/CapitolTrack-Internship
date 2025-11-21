054147e73abe204badc63459d4fbc1953dfe50a8
Victoria Hamman <vee10hamman@gmail.com>
2025-11-21 12:02:18 -0800

    Analysis by Party

--- Vote_Analysis.R
+++ Vote_Analysis.R
@@ -12,6 +12,7 @@ library(RSQLite)
 library(tidyverse)
 library(janitor)
 library(stringr)
+library(DBI)
 
 
 #---Connect to SQLite Database---#
@@ -40,6 +41,88 @@ member_data <- member_data %>%
 
 #---Descriptive Analysis---#
 
-#total number of measures passed
+#number of members
+m_nrow <- nrow(member_data)
+m_nrow
+#number of votes
+v_nrow <- nrow(vote_data)
+v_nrow
+
+#---Distribution of Measures---#
+
+#total pass vs. fail
+failed <- dbGetQuery(db, 'SELECT * FROM Votes WHERE outcome="F"')
+num_fail <- nrow(failed)
+num_fail
+
+passed <- dbGetQuery(db, 'SELECT * FROM Votes WHERE outcome="P"')
+num_pass <- nrow(passed)
+num_pass
+
+#pie chart
+dist <- c(num_fail, num_pass)
+pct <- round(dist/sum(dist)*100)
+labels <- paste(c("Failed", "Passed"), pct)
+pie(dist, labels=paste(labels, "%", sep=""), col=c('red','lightgreen'))
+
+
+#---Analysis by Party---#
+
+#make joined data by member ID
+joined_data <- dbGetQuery(db, 'SELECT v.vote_date, v.originator, v.outcome,
+               v.location_code, v.measure, v.author, v.member_id,
+               M.l_name, M.house, M.district, M.party
+               FROM Votes V LEFT JOIN Members M
+               ON V.member_id = M.member_id')
+dbWriteTable(db, "Joined_Data", joined_data, overwrite=T)
+dbListTables(db)
+dbGetQuery(db, 'SELECT * FROM Joined_Data LIMIT 10')
+
+##fail distribution
+dem_fails <- dbGetQuery(db, 'SELECT * FROM Joined_Data WHERE party="D" AND outcome="F"')
+dem_fails
+d_num_fails <- nrow(dem_fails)
+d_num_fails
+
+rep_fails <- dbGetQuery(db, 'SELECT * FROM Joined_Data WHERE party="R" AND outcome="F"')
+rep_fails
+r_num_fails <- nrow(rep_fails)
+r_num_fails
+
+#pie chart with percentages
+dist <- c(d_num_fails, r_num_fails)
+pct <- round(dist/sum(dist)*100)
+labels <- paste(c("Democrat Fails", "Republican Fails"),pct)
+pie(dist, labels=paste(labels,"%",sep=''), col=c("blue","red"))
+
+
+##pass distribution
+dem_pass <- dbGetQuery(db, 'SELECT * FROM Joined_Data WHERE party="D" AND outcome="P"')
+d_num_pass <- nrow(dem_pass)
+
+rep_pass <- dbGetQuery(db, 'SELECT * FROM Joined_Data WHERE party="R" AND outcome="P"')
+r_num_pass <- nrow(rep_pass)
+
+#pie chart with percentages
+dist <- c(d_num_pass, r_num_pass)
+pct <- round(dist/sum(dist)*100)
+labels <- paste(c("Democrat Passes", "Republican Passes"),pct)
+pie(dist, labels=paste(labels,"%",sep=''), col=c("blue","red"))
+
+
+#---Analysis on Author---#
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
