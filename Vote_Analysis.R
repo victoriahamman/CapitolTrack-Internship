e4ff896c1a32eca2e92dc459568bb8a9419a93c8
Victoria Hamman <vee10hamman@gmail.com>
2025-11-21 09:04:08 -0800

    Changes to Vote_Analysis

--- Vote_Analysis.R
+++ Vote_Analysis.R
@@ -1,9 +1,28 @@
 
-#Data Files
-Members <- read.csv("C:/Users/vee10/Downloads/CapitolTrack/Members.csv")
-Votes <- read.csv("C:/Users/vee10/Downloads/CapitolTrack/Votes.csv")
+#--- Load Data Files---#
+member_data <- read.csv("C:/Users/vee10/Downloads/CapitolTrack/Data/Members.csv")
+vote_data <- read.csv("C:/Users/vee10/Downloads/CapitolTrack/Data/Votes.csv")
 
-#Libraries
+head(vote_data)
+head(member_data)
+
+#---Libraries---#
 library(RSQLite)
-library(tidyr)
-library(dplyr)
+library(tidyverse)
+library(janitor)
+library(stringr)
+
+#--- Clean Data ---#
+
+#turns all column names to lowercase and adds underscores
+member_data <- member_data %>% clean_names()
+vote_data <- vote_data %>% clean_names()
+
+
+#remove vacancies/no member rows from member_data
+member_data <- member_data %>% 
+  filter(!str_detect(f_name, regex("vacancy|assigned", ignore_case=TRUE)))
+
+# Descriptive Analysis
+
+
